40a6246a6642a43cc7aab0020293a38af21ba69c
smadzelonka <smadzelonka@gmail.com>
2020-12-07 15:31:04 -0800

    updated fruits to objects

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const PORT = 4000;
 */
 
 /* ===== Home route ===== */
-// on a get request to "/" run a function
-// app.get("url", callback function(request{}, response{}){do something})
+    // on a get request to "/" run a function
+    // app.get("url", callback function(request{}, response{}){do something})
 app.get("/", function(req,res){
     res.send("Hello world")
 })
@@ -52,12 +52,28 @@ app.get("/", function(req,res){
 
 
 // fake database
-const fruits = ["apple","banana","orange"];
+const fruits = [
+    {
+        name:'apple',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name:'pear',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'banana',
+        color: 'yellow',
+        readyToEat: true
+    }
+];
 
 /* ==== Query ==== */
 // if a url has a questions mark what ever follows is a query
-//  "/fruits?name=Sean"
-// request.query = {name: "Sean"}
+    //  "/fruits?name=Sean"
+    // request.query = {name: "Sean"}
 
 // fruits index - show all fruits
     // Index  - GET       - /fruits       -> presentational
@@ -65,7 +81,7 @@ app.get("/fruits", function (req, res){
 
     if(req.query.filter){
         const filtered = fruits.filter((fruits =>{
-            return fruits.includes(req.query.filter)
+            return fruits.name.includes(req.query.filter)
         }))
         return res.send(filtered)
     }
@@ -81,26 +97,26 @@ app.get("/fruits/0", function (req, res){
 })
 
 /* ===== order matters ===== */
-// cool fruits route doesnt work because comes after the params app.get
+    // cool fruits route doesnt work because comes after the params app.get
 app.get("/fruits/cool", function (req, res){
     res.send("Fruits are cool")
 })
 // params
 app.get("/fruits/:index", function (req, res){
     const index = req.params.index
-    res.send(`This is your fruit ${fruits[index]}`)
+    res.send(`This is your fruit ${fruits[index].name}`)
 })
 // fruits with name
 app.get("/fruits/:index/:name", function (req, res){
     const index = req.params.index
-    res.send(`${req.params.name} is looking for ${fruits[index]}`)
+    res.send(`${req.params.name} is looking for ${fruits[index].name}`)
 })
 
 
 
 /* ====== sever bind ====== */
-// binds the application to the port via app.listen
-// app.listen(number, function to do after bind)
+    // binds the application to the port via app.listen
+    // app.listen(number, function to do after bind)
 app.listen(PORT, function(){
     console.log(`sever is live at http://localhost:${PORT}/`);
 })
