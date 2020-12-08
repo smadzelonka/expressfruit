670e7f046d4c4c85a2d95591bda4765cca921ed2
smadzelonka <smadzelonka@gmail.com>
2020-12-07 16:13:44 -0800

    new notes file

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ const app = express();
 /* ===== system variables ===== */
 const PORT = 4000;
 
-/* ===== Routes ===== */
-/* 
-    Request Methods
-        GET - get data
-        POST - create data -> body data
-        PUT - update data -> body data and target
-        PATCH - update data of a specfic value -> body data and target
-        DELETE - delete / destory -> target 
-*/
 
 /* ===== Home route ===== */
     // on a get request to "/" run a function
@@ -30,26 +21,6 @@ app.get("/", function(req,res){
 })
 
 // ==== fruit routes aka RESTFUL ROUTING ====
-
-
-/* 
-    CRUD
-        C-reate
-        R-ead
-        U-date
-        D-estory
-*/
-/* 
-    Restful routes
-    Index  - GET       - /fruits       -> presentational
-    New    - GET       - /fruits/new   -> Presentational Form
-    create - POST      - /fruits       -> Functional
-    show   - GET       - /fruits/index -> Presentational
-    edit   - GET       - /fruits/index -> Presentational Form
-    update - PUT/PATCH - /fruits/index -> Functional
-    delete - DELETE    - /fruits/index -> Funtional
-*/
-
 
 // fake database
 const fruits = [
---
+++ severNote.js
@@ -0,0 +1,27 @@
+/* ===== Routes ===== */
+/* 
+    Request Methods
+        GET - get data
+        POST - create data -> body data
+        PUT - update data -> body data and target
+        PATCH - update data of a specfic value -> body data and target
+        DELETE - delete / destory -> target 
+*/
+// ===== RestFul Routes ======
+/* 
+    CRUD
+        C-reate
+        R-ead
+        U-date
+        D-estory
+*/
+/* 
+    Restful routes
+    Index  - GET       - /fruits       -> presentational
+    New    - GET       - /fruits/new   -> Presentational Form
+    create - POST      - /fruits       -> Functional
+    show   - GET       - /fruits/index -> Presentational
+    edit   - GET       - /fruits/index -> Presentational Form
+    update - PUT/PATCH - /fruits/index -> Functional
+    delete - DELETE    - /fruits/index -> Funtional
+*/
